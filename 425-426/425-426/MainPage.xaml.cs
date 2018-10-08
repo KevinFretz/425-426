fadf18c3c33ec006f19b59e66209f159acc696ff
KevinFretz <43211705+KevinFretz@users.noreply.github.com>
2018-10-08 13:39:17 -0400

    Add files via upload

---
+++ 425-426/425-426/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Windows;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace _425_426
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BTN_Submit_Click(object sender, RoutedEventArgs e)
+        {
+            int[] presentState = new int[31] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            int[] previousState = new int[31];
+            int time = 0;
+
+
+            if (Yes_RadioB.IsChecked == true)
+            {
+                for (int i = 0; i < 31; i++)
+                {
+                    
+                }
+            }
+
+            DisplayPage display = new DisplayPage();
+            display.Show();
+        }
+    }
+}
